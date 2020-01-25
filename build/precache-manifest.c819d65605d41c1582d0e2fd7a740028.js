9bcb1e26b045ce346e8fb1d4bfd0dc52ade9f1d9
Santtu Lempinen <santtu.lempinen@helsinki.fi>
2020-01-25 17:19:33 +0200

    Remove build from gitignore

---
+++ build/precache-manifest.c819d65605d41c1582d0e2fd7a740028.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773b2e832f864309a3a1893779d7eb43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88b6fb0688e6dc2918c",
+    "url": "/static/js/2.08c9d303.chunk.js"
+  },
+  {
+    "revision": "af3c12ecbe51feea601dee34f9cabf5e",
+    "url": "/static/js/2.08c9d303.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ffa36536ec1cb31d3596",
+    "url": "/static/js/main.ff4f6566.chunk.js"
+  },
+  {
+    "revision": "296010a17b45c21632d0",
+    "url": "/static/js/runtime-main.b6387a0a.js"
+  }
+]);
